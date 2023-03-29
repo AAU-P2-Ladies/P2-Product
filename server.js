ef7b6bc309f92ecf744217fbcaa8739ff74d2655
AllanVester <vesterallan246@gmail.com>
2023-03-29 13:25:34 +0200

    Updated everything

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 const port = 3000
 
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
 
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'))
 
 })
 
-
-
 app.listen(port, () => {
 
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Server listening at http://70.34.205.8:${port}`)
 
 })
