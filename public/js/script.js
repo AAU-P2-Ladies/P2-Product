b6d94a2bea60086979dbe2ae04079ddfaaeb9b73
simonthordal <sthordal@gmail.com>
2023-03-31 08:18:48 +0200

    Added error message text & started on login func

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
-//extensions: emojis, css intellisense, express til server
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("log-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+
+loginButton.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    const username = loginForm.username-input.value;
+    const password = loginForm.password-input.value;
+
+
+});
