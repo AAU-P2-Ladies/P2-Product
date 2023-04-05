260e85083a81bf7531317a79010fa0952f6dc989
xmilla99 <kbja22@aau.student.dk>
2023-04-05 09:26:04 +0200

    back button works

--- public/js/coordinator.js
+++ public/js/coordinator.js
@@ -2,3 +2,7 @@ let back_btn = document.getElementById("back_btn");
 let save_btn = document.getElementById("save_btn");
 let unlock_btn = document.getElementById("unlock_btn");
 
+back_btn.addEventListener("click", function(){
+    console.log('clickededded');
+    window.location.href='/';
+})
