bd013bd105a57429ee110b58e623d60427fa3f36
xmilla99 <kbja22@aau.student.dk>
2023-04-17 14:30:33 +0200

    fix some snake

--- public/js/prefAlg.js
+++ public/js/prefAlg.js
@@ -44,7 +44,7 @@ function getRandomInts(max,block,numbers){
 //Define values that should be given by the website
 let min = 0;
 let StudentNum = 150;
-let group_size = 7;
+let groupSize = 7;
 
 //Initilize the student arrays with StudentNum amount of students 
 let students = [];
@@ -56,11 +56,11 @@ for (let s = 0;s<StudentNum;s++) {
 //Calculate whether the group member size adds up (groups can be made
 // with the group member size but some groups may have one less group member)  
 amountOfStudents = students.length;
-updated_group_size = Math.ceil(amountOfStudents/Math.ceil(amountOfStudents/group_size));
+updatedGroupSize = Math.ceil(amountOfStudents/Math.ceil(amountOfStudents/groupSize));
 
-//If it adds up. Updated_group_size will be the same as group_size
-if (updated_group_size != group_size) {
-    console.log("The group size does not add up. The new size is: ", updated_group_size)
+//If it adds up. updatedGroupSize will be the same as groupSize
+if (updatedGroupSize != groupSize) {
+    console.log("The group size does not add up. The new size is: ", updatedGroupSize)
 }
 
 //Updates the student array so each student have their index
@@ -70,7 +70,7 @@ indexStudents(students);
 let matrix = preferenceMatrix(students);
 
 //Returns the created groups
-let groups = pref_groups(students, matrix, updated_group_size);
+let groups = prefGroups(students, matrix, updatedGroupSize);
 
 /**
 * Creates a preferenceMatrix consisting of
@@ -94,11 +94,11 @@ for (let row = 0; row < numStudents; row++) {
     //calculations that have not been calculated yet 
     column_loop:
     for (let col = row; col < numStudents; col++) {
-        let pref_score = 0;
-        //Calculate the pref_score, consisting of: 
+        let prefScore = 0;
+        //Calculate the prefScore, consisting of: 
         //  student A's preference to student B  and 
         //  student B's preference to student A 
-        pref_score = pref_check(students[row], students[col], numStudents) + pref_check(students[col], students[row], numStudents)
+        prefScore = prefCheck(students[row], students[col], numStudents) + prefCheck(students[col], students[row], numStudents)
             
 
 /**
@@ -117,14 +117,14 @@ for (let row = 0; row < numStudents; row++) {
         }
 
         //In case both students does not have any common topics
-        //the pref_score will be raised with the number of students
+        //the prefScore will be raised with the number of students
         if (!commonTopics) {
-            pref_score += numStudents;
+            prefScore += numStudents;
         }
 
-        //The pref_score is assigned to both indexes in the matrix
-        preferenceMatrix[row][col] = pref_score;
-        preferenceMatrix[col][row] = pref_score;
+        //The prefScore is assigned to both indexes in the matrix
+        preferenceMatrix[row][col] = prefScore;
+        preferenceMatrix[col][row] = prefScore;
     }
 }
 return preferenceMatrix;
@@ -135,17 +135,17 @@ function indexStudents (students) {
 for (let indexNum = 1; indexNum < students.length; indexNum++) {
     students[indexNum].index = indexNum;
 }
-return students
+return students;
 }
 
 //findPrefSum is created to make the code more readable 
 //Returns the preference value of Student1 and Student2
-function find_pref_sum (student1, student2, matrix) {
+function findPrefSum (student1, student2, matrix) {
     return matrix[student1.index][student2.index];
 }
 
 
-function pref_check (student1, student2, student_number) {
+function prefCheck (student1, student2, student_number) {
     let preferenceIndex = student1.prefs.indexOf(student2.index);
     if (preferenceIndex >= 0) {
         return preferenceIndex + 1;
@@ -159,47 +159,47 @@ function pref_check (student1, student2, student_number) {
     }
 }
 
-function pref_groups (students, matrix, group_size) {  
-    const group_number = Math.ceil(students.length/group_size);
+function prefGroups (students, matrix, groupSize) {  
+    const groupNumber = Math.ceil(students.length/groupSize);
     
-    let smaller_groups = 0;
-    if ((students.length % group_size) != 0) {
+    let smallerGroups = 0;
+    if ((students.length % groupSize) != 0) {
 
-        smaller_groups = group_size - (students.length % group_size);
-        while (smaller_groups > group_number) {
-            group_size -= Math.floor(smaller_groups/group_number);
-            smaller_groups = group_size - (students.length % group_size);
+        smallerGroups = groupSize - (students.length % groupSize);
+        while (smallerGroups > groupNumber) {
+            groupSize -= Math.floor(smallerGroups/groupNumber);
+            smallerGroups = groupSize - (students.length % groupSize);
         }
     }
 
     let groups = []; 
-    for (let initialize = 0; initialize < group_number; initialize++) {
+    for (let initialize = 0; initialize < groupNumber; initialize++) {
         let group = new Group (new Array(2),false)
         groups.push(group)
     }
     
     let j = 0;
-    for (let groupNum = 0; groupNum < group_number; groupNum++) {
+    for (let groupNum = 0; groupNum < groupNumber; groupNum++) {
         while (students[j].group_nr != -1) {
             j++;
         }
         groups[groupNum].students[0] = students[j];
         students[j].group_nr = groupNum;
         //Creates an array without references in order to sort preference values
-        let sorted_studentToStudentsPrefs = matrix[j].slice();
+        let sortedStudentToStudentsPrefs = matrix[j].slice();
         //Sorts preference values in increasing order
-        sorted_studentToStudentsPrefs.sort((a, b) =>(a - b)); 
+        sortedStudentToStudentsPrefs.sort((a, b) =>(a - b)); 
         
         let a = 1
         let matrixIndex = -1
         do {            
-            if (sorted_studentToStudentsPrefs[a] != sorted_studentToStudentsPrefs[a-1]) {
+            if (sortedStudentToStudentsPrefs[a] != sortedStudentToStudentsPrefs[a-1]) {
                 matrixIndex = -1
             }
             //k gets assigned the indexOf the lowest preference value.
             //If the student is in a group the IndexOf searches for the next value
             //k + 1 allows to find the next student with same preference value in the matrix[j]
-            matrixIndex = matrix[j].indexOf(sorted_studentToStudentsPrefs[a], matrixIndex + 1);            
+            matrixIndex = matrix[j].indexOf(sortedStudentToStudentsPrefs[a], matrixIndex + 1);            
             a++
         } while (students[matrixIndex].group_nr != -1)
         
@@ -208,15 +208,15 @@ function pref_groups (students, matrix, group_size) {
     }
 
     let studentToGroupsPref = [];
-    for (let x = group_number; x < students.length; x++) {
+    for (let x = groupNumber; x < students.length; x++) {
         if (students[x].group_nr != -1) {
             continue;
         }
         let groupNum = 0
         let a = 0
-        studentToGroupsPref = new Array(group_number)
+        studentToGroupsPref = new Array(groupNumber)
 
-        for (groupNum; groupNum < group_number; groupNum++) {
+        for (groupNum; groupNum < groupNumber; groupNum++) {
             if (groups[groupNum].isFull) {
                 studentToGroupsPref[groupNum] = 0;
                 a++
@@ -226,23 +226,23 @@ function pref_groups (students, matrix, group_size) {
             studentToGroupsPref[groupNum] = 0;
             let j = 0;
 
-            for (j; j < group_size; j++) {
+            for (j; j < groupSize; j++) {
                 if (groups[groupNum].students[j] == undefined) {
                     break;
                 }
-                studentToGroupsPref[groupNum] += find_pref_sum(students[x], groups[groupNum].students[j], matrix);
+                studentToGroupsPref[groupNum] += findPrefSum(students[x], groups[groupNum].students[j], matrix);
             }
             studentToGroupsPref[groupNum] /= j;
         } 
         //Creates an array without references in order to sort preference values
-        let sorted_studentToGroupsPref = studentToGroupsPref.slice();
+        let sortedStudentToGroupsPref = studentToGroupsPref.slice();
         //Sorts preference values in increasing order
-        sorted_studentToGroupsPref.sort((a, b) =>(a - b)); 
-        groupNum = studentToGroupsPref.indexOf(sorted_studentToGroupsPref[a]);
+        sortedStudentToGroupsPref.sort((a, b) =>(a - b)); 
+        groupNum = studentToGroupsPref.indexOf(sortedStudentToGroupsPref[a]);
 
         groups[groupNum].students.push(students[x]);
         students[x].group_nr = groupNum;
-        if (groups[groupNum].students.length == group_size || (groupNum >= group_number - smaller_groups && groups[groupNum].students.length == group_size - 1)) {
+        if (groups[groupNum].students.length == groupSize || (groupNum >= groupNumber - smallerGroups && groups[groupNum].students.length == groupSize - 1)) {
             groups[groupNum].isFull = true;
         }
     }
@@ -254,7 +254,7 @@ function pref_groups (students, matrix, group_size) {
      */
     for (let i = 0; i < 0; i++) {
         console.log("Group:",i)
-        for (let j = 0; j < group_size; j++) {
+        for (let j = 0; j < groupSize; j++) {
             if (groups[i].students[j] != undefined) {
                 console.log(groups[i].students[j].firstName,groups[i].students[j].group_nr)
             }
@@ -327,7 +327,7 @@ function check_min_diversity(groups, min_diversity){
 function group_pref_avg(student, group, matrix){
     pref_sum = 0;
     for (let other_student of group.students){
-        pref_sum += find_pref_sum(student, other_student, matrix);
+        pref_sum += findPrefSum(student, other_student, matrix);
     }
     return pref_sum/group.students.length;
 }
