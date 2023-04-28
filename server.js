821c189411a8d9f80ca26e62e57de65fcb24e2ac
simonthordal <sthordal@gmail.com>
2023-04-28 08:34:00 +0200

    Added error handling middelware

--- server.js
+++ server.js
@@ -691,3 +691,14 @@ async function studentObjectMaker(users, semester){
     });
 
 }
+
+//These error middelware have to stay at the bottom
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  })
