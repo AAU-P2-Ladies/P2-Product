972f44933ffd72f261a143a4f13a637f19c8edb0
Meretekaldahl <meretekaldahlandersen@gmail.com>
2023-05-04 10:19:00 +0200

    student

--- public/js/student_profile.js
+++ public/js/student_profile.js
@@ -12,67 +12,90 @@ let nameArray = ["Adele","Agnes","Adrian","Adil","Andreas","Anders",
 
 
 let numberOfStudentPreferences = 3;
+let rolesIncluded = true;
+
 const list = document.getElementById("StudentProfile");
 let modalDiv = document.getElementById("modalContent");
 let roleTable = document.getElementById("rolesTable");
-let rolesIncluded = true;
+let button = document.getElementById("addPref");
+let error = document.getElementById("error");
+let modal = document.getElementById("prefModal");
+let overrideTextbox = document.getElementById("override");
 
-if(rolesIncluded){
+let checkboxes = document.getElementsByClassName("checkbox");
+let span = document.getElementsByClassName("close")[0];
+
+
+if (rolesIncluded) {
+
   roleTable.style.display = "";
-}else{
+
+} else {
+
   roleTable.style.display = "none";
+
 }
 
-/*
-if(rolesIncluded){
-  createRolesTable();
-}
+function checkboxControl(clickedCheckBox) {
 
-function createRolesTable(){
+  let total = 0;
 
-  let tableRowFirst = document.createElement("tr");
-  roleTable.append(tableRowFirst);
-  let tableHeading = document.createElement("th");
-  tableHeading.innerText = "Belbin Roles";
-  tableRowFirst.append(tableHeading);
+  for (var checkbox = 0; checkbox < checkboxes.length; checkbox++) {
 
-  for(let i = 1; i <= roles.length; i++){
-    let tableRow = document.createElement("tr");
-    let tableElement = document.createElement("td");
-    tableElement.innerText = roles[i];
-    roleTable.append(tableRow);
-    tableRow.append(tableElement);
+      if (checkboxes[checkbox].checked == true) {
+        total = total + 1;
+      }
+
+      if (total > 3) {
+        error.textContent = "You Can only Select Three Roles";
+        error.style.color = "red";
+        checkboxes[clickedCheckBox].checked = false;
+        return false;
+
+      }
 
   }
-  roleTable.style.display = "";
-}
-*/
 
-function createDropDown(id, array){
+}
+
+function createDropDown(id, array) {
+
   const selection = document.createElement("select");
   selection.id = id;
   selection.className = "selectt";
-  for(let i of array){
+
+  for (let i of array) {
+
     const options = document.createElement("option");
     const text = document.createTextNode(i);
     options.appendChild(text);
     options.value = i;
     selection.append(options);
+
   }
+
   return selection;
+
 }
 
-function makeBreaks(element, number){
+function makeBreaks(element, number) {
+
   let i = 0;
-  while(i < number){
+
+  while (i < number) {
+
     const breaks = document.createElement("br");
     element.after(breaks);
     i++;
+
   }
+
 }
 
-function priorities(number, data, nameId, divName){
-  for(let i = number; i > 0; i--){
+function priorities(number, data, nameId, divName) {
+
+  for (let i = number; i > 0; i--) {
+
     let selectionTopics = createDropDown((nameId+"Priority"+i), data);
     divName.prepend(selectionTopics);
     let currentSelection = document.getElementById(nameId+"Priority"+i);
@@ -82,69 +105,78 @@ function priorities(number, data, nameId, divName){
     nameLabel.appendChild(text);
     divName.prepend(nameLabel);
     makeBreaks(currentSelection, 1);
+
   }
+
   let headingE = document.createElement("h1");
   let textz = document.createTextNode(nameId + " Preferences");
   headingE.appendChild(textz);
   divName.prepend(headingE);
+
 }
 
-let newDiv = document.createElement("div");
-newDiv.id = "newDivOne";
-list.prepend(newDiv);
-priorities(numberOfStudentPreferences, topics, "Topic", newDiv);
+function addPrefFunction(e) {
 
-let btn = document.getElementById("addPref");
-let modal = document.getElementById("prefModal");
-let span = document.getElementsByClassName("close")[0];
-
-function addPrefFunction(e){
   e.preventDefault();
 
   modal.style.display = "block";
-  onclick="myFunction(event)";
+  onclick = "myFunction(event)";
+
 }
 
-span.onclick = function(){
+span.onclick = function() {
+
   modal.style.display = "none";
-}
-//den her virker ikke
-window.onclick = function(event){
-  if(event.target == modal){
-    modal.style.display = "none";
-  }
+
 }
 
-function saveStudentPreferences(e){
+//den her virker ikke
+window.onclick = function(event) {
+
+  if (event.target == modal) {
+
+    modal.style.display = "none";
+
+  }
+
+}
+
+function saveStudentPreferences(e) {
+
   e.preventDefault();
 
-    for(let priority = numberOfStudentPreferences; priority > 0; priority--){
+    for (let priority = numberOfStudentPreferences; priority > 0; priority--) {
     
-      if(document.getElementById("p" + priority) === null){
+      if (document.getElementById("p" + priority) === null) {
+
         let text = document.createElement("p");
         text.id = "p" + priority;
         let name = document.getElementById(priority + "prio");
         text.innerText = priority + ". Priority: " + name.value;
-        let button = document.getElementById("addPref");
         button.after(text);
 
-        btn.innerText = "Change student preference";
+        button.innerText = "Change student preference";
         modal.style.display = "none";
 
-      }else{
+      } else {
+
         let text = document.getElementById("p" + priority);
 
         let name = document.getElementById(priority + "prio");
         console.log(name.value);
         text.innerText = priority + ". Priority: " + name.value;
         
-        btn.innerText = "Change student preference";
+        button.innerText = "Change student preference";
         modal.style.display = "none";
+
       }
+
     }
+
 }
 
-function createSaveButton(){
+function createSaveButton() {
+
   let saveButton = document.createElement("button");
   saveButton.id = "SaveStudentPreferencesButton";
   saveButton.innerText = "Save";
@@ -154,18 +186,21 @@ function createSaveButton(){
   
 }
 
-function createDivs(numberOfDivs){
+function createDivs(numberOfDivs) {
 
-  for(div = 1; div <= numberOfDivs; div++){
+  for (div = 1; div <= numberOfDivs; div++) {
+
     let divElement = document.createElement("div");
     divElement.id = "prefDiv" + div;
     modalDiv.append(divElement);
     divElement.addEventListener("load", createDynamicList(divElement.id, div));
+
   }
   
 }
 
-function SearchField(myInputID, myULID){
+function SearchField(myInputID, myULID) {
+
   let input, filter, ul, li, txtValue;
   input = document.getElementById(myInputID);
   filter = input.value.toUpperCase();
@@ -175,31 +210,43 @@ function SearchField(myInputID, myULID){
   
   let count = 0;
     
-  for(let i = 0; li.length > i; i++){
+  for (let i = 0; li.length > i; i++) {
+
     txtValue = li[i].innerText;
       
-    if((txtValue.toUpperCase().indexOf(filter) > -1) && count < 10){
+    if ((txtValue.toUpperCase().indexOf(filter) > -1) && count < 10) {
+
       li[i].style.display = "block";
       count++;
-    }else{
+
+    } else {
+
         li[i].style.display = "none";
+
     }
+
   }
    
-  if(input.value == ""){
+  if (input.value == "") {
+
     ul.hidden = "hidden";
+
   }
+
 }
 
-function createDynamicList(id){   
+function createDynamicList(id) {   
+
   let divForList = document.getElementById(id);
   let list = document.createElement("ul");
   
-  for(let i in nameArray){
+  for (let i in nameArray) {
+
     let li = document.createElement('li');
     li.innerText = nameArray[i];
     li.className = "item";
     list.appendChild(li);
+
   }
 
   list.hidden = "";
@@ -208,9 +255,9 @@ function createDynamicList(id){
 
 }
 
-function createSearchPref(number){
+function createSearchPref(number) {
 
-  for(let currentNumber = 1; currentNumber <= number; currentNumber++){
+  for (let currentNumber = 1; currentNumber <= number; currentNumber++) {
 
     let inputText = document.createElement("input");
     inputText.setAttribute("type", "text");
@@ -233,37 +280,51 @@ function createSearchPref(number){
 
 }
 
+
+
+//KALDE FUNKTIONER
+
 createDivs(numberOfStudentPreferences);
+
 createSearchPref(numberOfStudentPreferences);
 
-for(let i = 1; i <= numberOfStudentPreferences; i++){
+for (let i = 1; i <= numberOfStudentPreferences; i++) {
+
   let test = document.getElementById(i + "prio");
   test.addEventListener("keyup", SearchField(test.id, "myUL" + i));
+
 }
 
-for(let input = 1; input <= numberOfStudentPreferences; input++){
+for (let input = 1; input <= numberOfStudentPreferences; input++) {
+
   document.getElementById("myUL" + input).addEventListener("click", function(e) {
+
     if (e.target && e.target.matches("li")) {
+
       let myInput = document.getElementById(input + "prio");
       myInput.value = e.target.innerText;
       document.getElementById("myUL" + input).hidden = "hidden";
+
     }
   
   });
+
 }
+
+let newDiv = document.createElement("div");
+newDiv.id = "newDivOne";
+list.prepend(newDiv);
+
+priorities(numberOfStudentPreferences, topics, "Topic", newDiv);
 
 createSaveButton();
 
-
-let overrideTextbox = document.getElementById("override");
-makeBreaks(overrideTextbox, 2);
+makeBreaks(overrideTextbox, 1);
 
 
 
 /*
 
-roller i tabel. krydse af. - maks 3. 
-
-hidden hvis coordinator ikke har krydset af.
+fjerne students fra listen hvis de er valgt
 
 */
